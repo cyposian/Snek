ac9eb8a2fac4fa5e8210559b75dbaf148173601b
Sam Cho <ysc996@gmail.com>
2022-07-05 23:38:09 -0400

    custom font

--- SidePanel.java
+++ SidePanel.java
@@ -1,26 +1,38 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics;
+import java.io.IOException;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class SidePanel {
 
-    String myFont, myFont2;
+    String bodyFont;
+    Font mainFont;
     DecimalFormat deci;
 
     public SidePanel() {
-        myFont = SnakePanel.myFont;
-        myFont2 = SnakePanel.myFont2;
+        bodyFont = "Monospaced";
         deci = SnakePanel.deci;
+
+        try {
+            InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream("./proxon/PROXON.ttf");
+            mainFont = Font.createFont(Font.TRUETYPE_FONT, stream).deriveFont(48f);
+         } catch (IOException e) {
+            e.printStackTrace();
+         } catch (FontFormatException e) {
+            e.printStackTrace();
+         }
     }
 
     public void draw(Graphics g) { // draw main panel
         g.setColor(Color.yellow);
-        g.setFont(new Font(myFont, Font.BOLD, 30));;
+        g.setFont(mainFont.deriveFont(Font.BOLD, 30f));;
         g.drawString("Snek", 620, 80);
   
         g.setColor(Color.white);
-        g.setFont(new Font(myFont2, Font.PLAIN, 18));
+        g.setFont(new Font(bodyFont, Font.PLAIN, 18));
         g.drawString("Score: " + SnakePanel.score, 530, 120);
         g.drawString("Fruits: " + SnakePanel.fruits, 530, 150);
         g.drawString("Value: " + SnakePanel.fruitWorth, 530, 180);
@@ -32,14 +44,14 @@ public class SidePanel {
         g.drawString("Left/A", 530, 390);
         g.drawString("Right/D", 530, 420);
         g.drawString("Pause:P", 530, 450);
-        g.setFont(new Font(myFont, Font.PLAIN, 18));
+        g.setFont(mainFont.deriveFont(18f));
         g.drawString("Controls :", 530, 300);
         g.drawString("Difficulty :", 670, 300);
     }
 
     public void drawStartScreen(Graphics g) {
         g.setColor(Color.cyan);
-         g.setFont(new Font(myFont, Font.PLAIN, 35));
+         g.setFont(mainFont.deriveFont(35f));
          g.drawString("Select a difficulty", 95, 225);
          g.drawString("to start", 185, 295);
     }
@@ -47,18 +59,18 @@ public class SidePanel {
     public void drawEndScreen(Graphics g) {
         if(SnakePanel.score > SnakePanel.high) {
             SnakePanel.high = SnakePanel.score;
-            g.setFont(new Font(myFont, Font.BOLD, 40));
+            g.setFont(mainFont.deriveFont(Font.BOLD, 40f));
             g.setColor(Color.orange);
             g.drawString("NEW HIGH SCORE !", 60, 180);
             g.setColor(Color.red);
             g.drawString("GAME OVER", 135, 255);
-            g.setFont(new Font(myFont, Font.PLAIN, 35));
+            g.setFont(mainFont.deriveFont(35f));
             g.drawString("Press Enter to Restart", 55, 330);
          } else {
             g.setColor(Color.red);
-            g.setFont(new Font(myFont, Font.BOLD, 38));
+            g.setFont(mainFont.deriveFont(Font.BOLD, 38f));
             g.drawString("GAME OVER", 135, 200);
-            g.setFont(new Font(myFont, Font.PLAIN, 35));
+            g.setFont(mainFont.deriveFont(35f));
             g.drawString("Press Enter to Restart", 45, 275);
          }
     }
--- SnakePanel.java
+++ SnakePanel.java
@@ -1,6 +1,5 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
@@ -9,11 +8,9 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.PrintStream;
 import java.text.DecimalFormat;
 import java.util.Scanner;
-
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -27,7 +24,6 @@ public class SnakePanel extends JPanel implements Runnable {
    private final int cellWidth = 20;
    public static int direction, score, high, fruits, fruitWorth, ms;
    public static DecimalFormat deci;
-   public static String myFont, myFont2;
    private BufferedImage myImage;
    private boolean isPaused, isPlaying, startScreen, keyUp, keyDown, keyLeft, keyRight;
    private int startPos, difficulty;
@@ -47,8 +43,6 @@ public class SnakePanel extends JPanel implements Runnable {
       this.addKeyListener(new KeyHandler());
       this.setFocusable(true);
       myImage = new BufferedImage(825, 541, BufferedImage.TYPE_INT_RGB);
-      myFont = "Proxon"; // Header font   // IMPORTING FONT IF NOT INSTALLED***
-      myFont2 = "Monospaced"; // Body font
       startScreen = true;
       isPaused = isPlaying = false;
       fruitWorth = 100;
@@ -56,9 +50,6 @@ public class SnakePanel extends JPanel implements Runnable {
       deci = new DecimalFormat("0.00");
       spencer = new Snake(startPos, snakeLength);
       sidePanel = new SidePanel();
-
-      InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream("./proxon/PROXON.ttf")
-      Font font = Font.createFont(Font.TRUETYPE_FONT, stream).deriveFont(48f)
 
       try {
          infile = new Scanner(new File("snakeScore.txt"));   		
